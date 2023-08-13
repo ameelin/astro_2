328b9c8433ec00035ad8c7cf3e380bc611eb54bf
Neal Ravindran <nravindran@sherpa6.com>
2023-08-13 00:39:33 -0400

    4th commit - the login form is now very pretty

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,6 +7,9 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  showPassword = false;
   constructor() { }
 
   onLoginFormSubmit(loginForm: NgForm): void {
@@ -14,5 +17,9 @@ export class LoginComponent {
     console.log('Password:', loginForm.value.password);
     console.log('Form submitted');
     
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
   }
 }
